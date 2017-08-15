04a22deba7345fe8d67a3cd24ab8d513e8b9d1f8
aninaslyan <aninaslyan84@gmail.com>
2017-08-15 18:03:44 +0400

    new version

--- movies.api/app.js
+++ movies.api/app.js
@@ -1,15 +1,16 @@
 var express = require('express');
-var moviesRouter = require('./routes/movies');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+var moviesRouter=require('./routes/moviesRouter');
+var mongoose=require('mongoose');
+var bodyParser=require('body-parser');
 
-var db = mongoose.connect("mongodb://localhost/MoviesDb");
+var db=mongoose.connect("mongodb://localhost/MoviesDb");
+
 
 var app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, function () {
-    console.log("server is running on 3000");
+app.listen(8080, function () {
+    console.log("server is running on 8080");
 });
 
 app.use('/movies',moviesRouter);
