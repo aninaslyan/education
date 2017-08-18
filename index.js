41dbd524767944240065997af26c44d141dcd804
aninaslyan <aninaslyan84@gmail.com>
2017-08-18 18:11:09 +0400

    Get, Post

--- index.js
+++ index.js
@@ -8,28 +8,3 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', emailRout);
 app.listen(3000);
-
-
-
-
-// Email.find(function (err, todos) {
-//     if (err) return console.error(err);
-//     console.log(todos);
-// });
-
-//
-// const MongoClient = require('mongodb').MongoClient;
-// const url = "mongodb://localhost:27017/mydb";
-//
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     var myobj = { email: "myemail@gmail.com", subject: "new email", body: "comment of the body",
-//                   scheduleDateTime: Date.now() };
-//     db.collection("email").insertOne(myobj, function(err, doc) {
-//         if (err) throw err;
-//         //console.log("1 document inserted");
-//         console.log(doc);
-//         db.close();
-//     });
-// });
-
